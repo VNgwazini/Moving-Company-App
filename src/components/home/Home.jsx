8b10df76c451645c389dac4666f47dce0b917e23
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-01-21 05:21:53 -0800

    Added testimonial slider.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,6 +6,8 @@ import {
  Button,
  Carousel
 } from 'react-bootstrap/'
+import Testimonial from 'react-testimonial';
+
 
 export default function Home() {
  
@@ -90,6 +92,56 @@ export default function Home() {
           <p className="text-center">
           Company Logo 6
           </p>
+        </Col>
+      </Row>
+    </Container>
+    <Container className="content-container testimonials py-4" fluid="true">
+      <Row>
+        <Col>
+          <Testimonial>
+                <div className="card content">
+                  <div className="card-body text-center">
+                    <img
+                    className="py-2"
+                    src="https://lh3.googleusercontent.com/a-/AOh14Gh9B93AFDIVhCHQfKm7rfB4qzRYJAsHBNeSklk9zg=w90-h90-p-rp-mo-br100"
+                    >
+                    </img>
+                    <h3 className="py-2">Samuel Ekong</h3>
+                    <p>
+                      Our experience with the Nitro Moving Company was excellent. 
+                      We are completely satisfied.  
+                    </p>
+                  </div>
+                </div>
+                <div className="card content">
+                  <div className="card-body text-center">
+                  <img
+                  className="py-2"
+                  src="https://lh3.googleusercontent.com/a-/AOh14Gi0HGO8VARvl7tJafdvZ26K3u9yeyTUrfUi0Q3BKw=w90-h90-p-rp-mo-br100"
+                >
+                </img>
+                <h3 className="py-2">John O'Brien</h3>
+                <p>
+                  I used them for my move in Lake Arrowhead to San Bernardino and it was AWESOME. 
+                </p>
+                  </div>
+                </div>
+                <div className="card content">
+                  <div className="card-body text-center">
+                  <img
+                  className="py-2"
+                  src="https://lh3.googleusercontent.com/a-/AOh14GgHeevMaEqa2DKuCStuXI-kJMxV4DMX3kZlYM5bZQ=w90-h90-p-rp-mo-br100"
+                >
+                </img>
+                <h3 className="py-2">Kat DeLong</h3>
+                <p>
+                  Nigel and his crew were very professional, 
+                  handled our furniture with care (wrapped every piece), 
+                  courteous and thoughtful. 
+                </p>
+                  </div>
+                </div>
+            </Testimonial>
         </Col>
       </Row>
     </Container>
