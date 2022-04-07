afba7810966160a1ce4e2b7f9052f15d50adeb20
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2022-04-06 19:54:23 -0700

    #5 Added payment compent and QR Codes.

--- src/data.js
+++ src/data.js
@@ -2,8 +2,8 @@ export const paymentData = [
   {
     id: 2,
     title: "Venmo",
-    link: "https://venmo.com/code?user_id=2477025851867136928&created=1647282858.8186212",
-    text: "Send payments to: @user-name",
+    link: "https://venmo.com/code?user_id=2739533162479616678&created=1649297678.683289&printed=1",
+    text: "Send payments to: @NitroCo",
     footer: "US Dollar",
     imgSrc: "assets/QR_Venmo.jpg",
     imgAlt: "Venmo",
@@ -11,8 +11,8 @@ export const paymentData = [
   {
     id: 6,
     title: "CashApp",
-    link: "https://cash.app/$vusajazz?qr=1",
-    text: "Send payments to: $username",
+    link: "https://cash.app/$Nitromc",
+    text: "Send payments to: $Nitromc",
     footer: "US Dollar",
     imgSrc: "assets/QR_CashApp.jpg",
     imgAlt: "CashApp",
@@ -20,10 +20,10 @@ export const paymentData = [
   {
     id: 4,
     title: "Zelle",
-    link: "https://enroll.zellepay.com/qr-codes?data=ewogICJuYW1lIiA6ICJWVVNVTVVaSSIsCiAgInRva2VuIiA6ICJ2dXN1bXV6aS5uZ3dhemluaUBnbWFpbC5jb20iLAogICJhY3Rpb24iIDogInBheW1lbnQiCn0=",
-    text: "Send payments to: username@email.com",
+    link: "https://enroll.zellepay.com/qr-codes?data=eyJuYW1lIjoiTklUUk8gTU9WSU5HIENPTVBBTlkgTExDIiwiYWN0aW9uIjoicGF5bWVudCIsInRva2VuIjoibml0cm9tb3Zpbmdjb21wYW55QGdtYWlsLmNvbSJ9",
+    text: "Send payments to: nitromovingcompany@gmail.com",
     footer: "US Dollar",
-    imgSrc: "assets/QR_Zelle2.PNG",
+    imgSrc: "assets/QR_Zelle.jpg",
     imgAlt: "Zelle",
   },
 ];
